6ce4b9eae946e50af7ce15069c4aea79291def17
jorgeedvaldo <edvaldojorge1996@gmail.com>
2020-10-07 18:57:03 +0100

    Mudando texto

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,7 +56,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        Manter Logado
                                     </label>
                                 </div>
                             </div>
