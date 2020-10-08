5545736b12ccb0b02bfafbe9a5ba8a7a74246d8c
jorgeedvaldo <edvaldojorge1996@gmail.com>
2020-10-08 19:01:55 +0100

    Mostrando fundo nas encomendas

--- resources/views/encomendas.blade.php
+++ resources/views/encomendas.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+<section class="hero-wrap hero-wrap-2 mb-5" style="background-image: url('images/bg_4.jpg');" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row no-gutters slider-text align-items-center justify-content-center">
+        <div class="col-md-9 ftco-animate text-center">
+          <h1 class="mb-2 bread">Encomendas</h1>
+        <p class="breadcrumbs"><span class="mr-2"><a href="{{url('/')}}">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Encomendas <i class="ion-ios-arrow-forward"></i></span></p>
+        </div>
+      </div>
+    </div>
+</section>
 <div class="container">
     <div class="row justify-content-center">
         @if(isset($isSaved))
@@ -12,20 +23,32 @@
             @foreach($DadosEncomenda as $encomenda)
                 <div class="card mb-3">
                     <div class="card-header">
-                    De: {{ $encomenda['DataHoraFeita'] }}
+                    De: {{ date('d/M/Y H:i:s',  strtotime($encomenda['DataHoraFeita'])) }}
                     </div>
                     <div class="card-body">
                         <h5 class="card-title">Este Pedido Contém:</h5>
                         <p class="card-text">| @foreach($encomenda['pedidoprodutos'] as $PedidoProduto) {{$produtoController->getById($PedidoProduto['IdProduto'])[0]['Nome']}} ({{$PedidoProduto['Quantidade']}}) | @endforeach</p>
-
+                        <h4>Total: {{$encomenda['Total']}} KZ</h4>
                         @if(strcmp($encomenda['Estado'], "Cancelado") == 0)
                             <div class="alert alert-danger mt-3 mb-3">Pedido Cancelado.</div>
-                        @else
+                        @elseif(strcmp($encomenda['Estado'], "Não Atendido") == 0)
+                            <div class="alert mt-3 mb-3">Pedido Não Atendido no Momento.</div>
                             <button id="BtnCancelarEncomenda" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" onclick="document.getElementById('EncomendaId').value = '{{$encomenda['Id']}}'">Cancelar Encomenda</button>
+                        @elseif(strcmp($encomenda['Estado'], "Em Atendimento") == 0)
+                         <div class="alert alert-warning mt-3 mb-3">Pedido Em Atendimento.</div>
+                         <button id="BtnCancelarEncomenda" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" onclick="document.getElementById('EncomendaId').value = '{{$encomenda['Id']}}'">Cancelar Encomenda</button>
+                        @elseif(strcmp($encomenda['Estado'], "Entregue") == 0)
+                          <div class="alert alert-success mt-3 mb-3">Este Pedido foi Entregue</div>
                         @endif
                     </div>
                 </div>
             @endforeach
+
+            @if( count($DadosEncomenda) == 0)
+                <div class="col-md-12 m-5 p-5">
+                    <h1>Você não Solicitou Nenhum até Agora...</h1>
+                </div>
+            @endif
         </div>
     </div>
 </div>
