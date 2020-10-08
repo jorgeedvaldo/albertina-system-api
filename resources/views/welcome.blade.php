6a0877b2923464f857cc5eb5652c4755a0798d23
jorgeedvaldo <edvaldojorge1996@gmail.com>
2020-10-08 19:02:14 +0100

    Textos para a home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,23 +12,25 @@
             </div>
             <div class="one-forth d-flex js-fullheight align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
                 <div class="text mt-md-5">
-                  <h1 class="mb-4">Eat Healthy <br> and Natural Foods</h1>
-                  <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country.</p>
-                  <div class="thumb mt-4 mb-3 d-flex">
-                      <a href="#" class="thumb-menu pr-md-4 text-center">
-                          <div class="img" style="background-image: url(images/menu-1.jpg);"></div>
-                          <h4>Australian Organic Beef</h4>
-                      </a>
-                      <a href="#" class="thumb-menu pr-md-4 text-center">
-                          <div class="img" style="background-image: url(images/menu-2.jpg);"></div>
-                          <h4>Australian Organic Beef</h4>
-                      </a>
-                      <a href="#" class="thumb-menu pr-md-4 text-center">
-                          <div class="img" style="background-image: url(images/menu-3.jpg);"></div>
-                          <h4>Australian Organic Beef</h4>
-                      </a>
-                  </div>
-                  <p><a href="#" class="btn btn-primary px-4 py-3 mt-3">Book A Table</a></p>
+                    <h1 class="mb-4">Comida Crocante <br> e Natural</h1>
+                  <p>A Albertina & Filhos traz um cardápio elaborado com um toque inovador, distinguido na
+                    confecção com excelência e nas rápidas entregas. A Albertina & Filhos é mais que uma
+                    pastelaria comum, os nossos serviços de cofeteria são os melhores já vistos.</p>
+                    <div class="thumb mt-4 mb-3 d-flex">
+                        <a href="#" class="thumb-menu pr-md-4 text-center">
+                            <div class="img" style="background-image: url(images/menu-1.jpg);"></div>
+                            <h4>Hamburgers</h4>
+                        </a>
+                        <a href="#" class="thumb-menu pr-md-4 text-center">
+                            <div class="img" style="background-image: url(images/menu-2.jpg);"></div>
+                            <h4>Cervejas nacionais</h4>
+                        </a>
+                        <a href="#" class="thumb-menu pr-md-4 text-center">
+                            <div class="img" style="background-image: url(images/menu-3.jpg);"></div>
+                            <h4>Melhores pizzas</h4>
+                        </a>
+                    </div>
+                    <p><a href="/loja" class="btn btn-primary px-4 py-3 mt-3">Fazer Pedido</a></p>
               </div>
             </div>
           </div>
@@ -44,23 +46,25 @@
             </div>
             <div class="one-forth d-flex js-fullheight align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
                 <div class="text mt-md-5">
-                  <h1 class="mb-4">We Love <br> Delicious Foods</h1>
-                  <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country.</p>
-                  <div class="thumb mt-4 mb-3 d-flex">
-                      <a href="#" class="thumb-menu pr-md-4 text-center">
-                          <div class="img" style="background-image: url(images/menu-1.jpg);"></div>
-                          <h4>Australian Organic Beef</h4>
-                      </a>
-                      <a href="#" class="thumb-menu pr-md-4 text-center">
-                          <div class="img" style="background-image: url(images/menu-2.jpg);"></div>
-                          <h4>Australian Organic Beef</h4>
-                      </a>
-                      <a href="#" class="thumb-menu pr-md-4 text-center">
-                          <div class="img" style="background-image: url(images/menu-3.jpg);"></div>
-                          <h4>Australian Organic Beef</h4>
-                      </a>
-                  </div>
-                  <p><a href="#" class="btn btn-primary px-4 py-3 mt-3">Book A Table</a></p>
+                    <h1 class="mb-4">Nós Amamos <br> Comidas Deliciosas</h1>
+                  <p>A Albertina & Filhos traz um cardápio elaborado com um toque inovador, distinguido na
+                    confecção com excelência e nas rápidas entregas. A Albertina & Filhos é mais que uma
+                    pastelaria comum, os nossos serviços de cofeteria são os melhores já vistos.</p>
+                    <div class="thumb mt-4 mb-3 d-flex">
+                        <a href="#" class="thumb-menu pr-md-4 text-center">
+                            <div class="img" style="background-image: url(images/menu-1.jpg);"></div>
+                            <h4>Hamburgers</h4>
+                        </a>
+                        <a href="#" class="thumb-menu pr-md-4 text-center">
+                            <div class="img" style="background-image: url(images/menu-2.jpg);"></div>
+                            <h4>Cervejas nacionais</h4>
+                        </a>
+                        <a href="#" class="thumb-menu pr-md-4 text-center">
+                            <div class="img" style="background-image: url(images/menu-3.jpg);"></div>
+                            <h4>Melhores pizzas</h4>
+                        </a>
+                    </div>
+                    <p><a href="/loja" class="btn btn-primary px-4 py-3 mt-3">Fazer Pedido</a></p>
               </div>
             </div>
           </div>
@@ -78,25 +82,27 @@
                   <div class="col-sm-7 wrap-about py-5 ftco-animate">
             <div class="heading-section mt-5 mb-4">
                 <div class="pl-lg-5 ml-md-5">
-                    <span class="subheading">About</span>
-                  <h2 class="mb-4">Welcome to Kusina Restaurant</h2>
+                    <span class="subheading">Sobre</span>
+                  <h2 class="mb-4">Bem vindo ao Albertina & Filhos</h2>
               </div>
             </div>
             <div class="pl-lg-5 ml-md-5">
-                          <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-                          <h3 class="mt-5">Special Recipe</h3>
+                          <p>A “Albertina & Filhos, Pastelaria e Fastfood” teve a sua abertura oficialmente nos meados de 2014, está situada nos combatentes, próximo à Escola 147. O espaço actual da pastelaria antes era usado para outros fins. A dona Albertina, sentiu que há necessidade de expandir a sua área financeira e decidiu então abrir a Pastelaria.
+
+                            Três anos depois a dona da Empresa, decidiu expandir seus produtos e passou também a comercializar Fastfood (Hambúrguer, Pizza, etc), e devido a demasiada aderência solicitou-se então um sistema capaz de realizar encomendas por um Website.</p>
+                          <h3 class="mt-5">Especial da Casa</h3>
                           <div class="thumb my-4 d-flex">
                   <a href="#" class="thumb-menu pr-md-4 text-center">
                       <div class="img" style="background-image: url(images/lunch-4.jpg);"></div>
-                      <h4>Australian Organic Beef</h4>
+                      <h4>Magoga - Frango com Batatas</h4>
                   </a>
                   <a href="#" class="thumb-menu pr-md-4 text-center">
                       <div class="img" style="background-image: url(images/lunch-7.jpg);"></div>
-                      <h4>Australian Organic Beef</h4>
+                      <h4>Torre de Cerveja Luandina</h4>
                   </a>
                   <a href="#" class="thumb-menu pr-md-4 text-center">
                       <div class="img" style="background-image: url(images/lunch-6.jpg);"></div>
-                      <h4>Australian Organic Beef</h4>
+                      <h4>Hambúrguer Suculento</h4>
                   </a>
               </div>
                       </div>
@@ -148,13 +154,12 @@
       </div>
   </section>
 
-
   <section class="ftco-section">
       <div class="container">
           <div class="row justify-content-center mb-5 pb-2">
         <div class="col-md-7 text-center heading-section ftco-animate">
-            <span class="subheading">Specialties</span>
-          <h2 class="mb-4">Our Menu</h2>
+            <span class="subheading">Especialidades</span>
+          <h2 class="mb-4">Nosso Menu</h2>
         </div>
       </div>
       <div class="row">
@@ -302,7 +307,7 @@
           </div>
           </div>
 
-          <!--  -->
+
           <div class="col-md-6 col-lg-4 menu-wrap">
               <div class="heading-menu text-center ftco-animate">
                   <h3>Desserts</h3>
@@ -449,10 +454,10 @@
       </div>
       <div class="row">
           <div class="col-md-12 text-center ftco-animate">
-              <p><a href="#" class="btn btn-black py-3 px-5">View All Menu</a></p>
+              <p><a href="/loja" class="btn btn-black py-3 px-5">Ver todos os Produtos</a></p>
           </div>
       </div>
-      </div>
+    </div>
 
   </section>
 @endsection
